07a987e09b8a38d712e24c8ca3cf6a3344e0166a
Umme-Nahida <mysuccessmission@gmail.com>
2024-02-17 19:33:42 +0600

    create header and footer

--- src/Pages/Component/Address/Address.jsx
+++ src/Pages/Component/Address/Address.jsx
@@ -1,12 +1,29 @@
 import { SlCalender } from "react-icons/sl";
+import { FaPhoneVolume } from "react-icons/fa6";
+import { IoLocationSharp } from "react-icons/io5";
+
 const Address = () => {
     return (
-        <div className="bg-black">
-            <div className="flex gap-2">
-                <SlCalender></SlCalender>
+        <div className="bg-black flex items-center justify-evenly py-20 mb-5">
+            <div className="flex justify-center items-center gap-5">
+                <SlCalender className="text-white text-4xl"></SlCalender>
                 <div>
-                    <p>We are open monday-friday</p>
-                    <h3>7:00 am - 9:00 pm</h3>
+                    <p className="text-white">We are open monday-friday</p>
+                    <h3 className="text-white text-2xl">7:00 am - 9:00 pm</h3>
+                </div>
+            </div>
+            <div className="flex justify-center items-center gap-5">
+                <FaPhoneVolume className="text-white text-4xl"></FaPhoneVolume>
+                <div>
+                    <p className="text-white">Have a question?</p>
+                    <h3 className="text-white text-2xl">+2546 251 2658</h3>
+                </div>
+            </div>
+            <div className="flex justify-center items-center gap-5">
+                <IoLocationSharp className="text-white text-4xl"></IoLocationSharp>
+                <div>
+                    <p className="text-white">Need a repair? our address</p>
+                    <h3 className="text-white text-2xl">Liza Street, New York</h3>
                 </div>
             </div>
            
--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -2,7 +2,7 @@ import logo from '../../../public/image/logo.svg';
 
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-base-200 text-base-content">
+        <footer className="footer p-10 bg-black  text-white">
             <aside>
                 <img src={logo} alt="" />
                 <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
